1db9875af7ce2f23452dfdf7e1bfb4348978232a
xixiaohui <yamazake@yeah.net>
2025-11-30 22:13:56 +0800

    add test

---
+++ app/test/page.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+export default function TestPage() {
+  return (
+    <>
+      <Box>
+        <Typography gutterBottom variant="h1">
+          环巢湖骑行Club
+        </Typography>
+      </Box>
+    </>
+  );
+}
