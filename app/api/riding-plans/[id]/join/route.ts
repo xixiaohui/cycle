cd90021ea5d203b4bba9288287efb1050bad8cd7
Frank xi <yamazake@yeah.net>
2026-01-26 14:09:43 +0800

    ddd

--- app/api/riding-plans/[id]/join/route.ts
+++ app/api/riding-plans/[id]/join/route.ts
@@ -4,18 +4,19 @@ import pool from "@/lib/db";
 
 export async function POST(
   req: Request,
-  { params }: { params: { id: string } }
+  context : { params: Promise<{ id: string }> }
 ) {
 //   const userId = getUserId(req);
 //   if (!userId) return new Response("Unauthorized", { status: 401 });
 
+  const { id } = await context.params;
   await pool.query(
     `
     INSERT INTO riding_plan_participants (plan_id, user_id)
     VALUES ($1, $2)
     ON CONFLICT DO NOTHING
     `,
-    [params.id, "userId"]
+    [id, "userId"]
   );
 
   return Response.json({ success: true });
